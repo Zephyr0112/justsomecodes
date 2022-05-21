fb0670d2211a7aea2d2619447bd7cb540041676c
Sugat Shanker <57261729+Zephyr0112@users.noreply.github.com>
2022-05-22 00:47:22 +0530

    Create evaluateexpression.py

---
+++ evaluateexpression.py
@@ -0,0 +1,21 @@
+class Solution:
+	# @param A : list of strings
+	# @return an integer
+	def evalRPN(self, A):
+		tokens = A
+		stack = []
+        for t in tokens:
+            if t not in "+-*/":
+                stack.append(int(t))
+            else:
+                r, l = stack.pop(), stack.pop()
+                if t == "+":
+                    stack.append(l+r)
+                elif t == "-":
+                    stack.append(l-r)
+                elif t == "*":
+                    stack.append(l*r)
+                else:
+                    stack.append(int(float(l)/r))
+        return stack.pop()
+     
